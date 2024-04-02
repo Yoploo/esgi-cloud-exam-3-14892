b8280e98b43a9f3935507a7c1118221521779139
Yoploo <thierry.yoran@gmail.com>
2024-04-02 15:10:41 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'RZAACKGGK21DZ58NEZT9', // TODO
+    secretAccessKey: 'z2j9HsmJCzZCMLRXhFgFsuMLDSVUDoD8ajiSW5Em', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-14892', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
